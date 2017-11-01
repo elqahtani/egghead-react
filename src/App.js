9050c376c7a6bd0f9ffb8892a2c1a1028a844a5c
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-11-01 13:34:13 +0700

    Add egghead example project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return (
+    <div>
+    <h1>Hi bro</h1> 
+      <p>Gue banget</p>
+      <h1> Mantab bray </h1>
+      <b>Test</b>
+   </div>
+   )
+  }
+}
+
+// const App = () => <h1>Hello Stateless</h1>
+
+
+export default App
